a47355247c7c21df6398e8c5a7f6b687d8596029
uttarayan21 <email@uttarayan.me>
2024-03-28 19:21:02 +0530

    feat(rust): Added a rust template

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  description = ''
+    A opinionated, simple(?) nix flake
+  '';
+  outputs = {self}: {
+    templates = {
+      rust = {
+        path = ./rust;
+        description = "A simple rust template using craneLib";
+      };
+    };
+  };
+}
